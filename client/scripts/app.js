6c572cd2502e095a38e4fe2217f8bd4629af19a5
d-kang <david.kang714@gmail.com>
2017-07-14 18:36:54 -0700

    Refactor to Shorthand getJSON

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -4,35 +4,46 @@ var app = {};
 app.init = function() {};
 app.send = function() {};
 
+// var url =  'http://parse.hrr.hackreactor.com/chatterbox/classes/messages&callback=?';
+// var new_url = "http://cors.io/?u=" + encodeURIComponent( url );
 
-$(function() {
-  // https://stackoverflow.com/questions/36819223/403-forbidden-response-to-a-jsonp-request
-  var url =  'http://parse.hrr.hackreactor.com/chatterbox/classes/messages';
-  $.ajax({
-    type: 'GET',
-    url: url,
-    // dataType: 'jsonp',
-    success: function(data) {
-      console.log('GET request successful', JSON.stringify(data), data);
-      $.each(data.results, function(i, info) {
-        var node = document.createElement('li');
-        var date = new Date(info.createdAt);
-        var formattedMessage = `${info.username}: ${info.text} ${date.toLocaleTimeString()}`;
-        var textNode = document.createTextNode(formattedMessage);
-        node.appendChild(textNode);
-        //
-        document.getElementById('messages').appendChild(node);
-        // $('.addMessage').append(`<li>${formattedMessage}</li>`)
-      })
-    },
-    error: function() {
-      console.log('error loading GET request');
-    }
-  })
+var url =  'http://parse.hrr.hackreactor.com/chatterbox/classes/messages';
 
-
-
+$.ajaxSetup({ dataType: "jsonp" });
+$.getJSON(url, function(json) {
+  $.each(json.results, function(i, info) {
+    var date = new Date(info.createdAt);
+    var formattedMessage = `${info.username}: ${info.text} ${date.toLocaleTimeString()}`;
+    $('#messages').append(`<li>${formattedMessage}</li>`);
+  });
 });
+
+
+// $(function() {
+//   // https://stackoverflow.com/questions/36819223/403-forbidden-response-to-a-jsonp-request
+//   var url =  'http://parse.hrr.hackreactor.com/chatterbox/classes/messages';
+//   $.ajax({
+//     type: 'GET',
+//     url: url,
+//     // dataType: 'jsonp',
+//     success: function(data) {
+//       console.log('GET request successful', JSON.stringify(data), data);
+//       $.each(data.results, function(i, info) {
+//         var node = document.createElement('li');
+//         var date = new Date(info.createdAt);
+//         var formattedMessage = `${info.username}: ${info.text} ${date.toLocaleTimeString()}`;
+//         var textNode = document.createTextNode(formattedMessage);
+//         node.appendChild(textNode);
+//         //
+//         document.getElementById('messages').appendChild(node);
+//         // $('.addMessage').append(`<li>${formattedMessage}</li>`)
+//       })
+//     },
+//     error: function() {
+//       console.log('error loading GET request');
+//     }
+//   })
+// });
 
 // example data
 var data = {
